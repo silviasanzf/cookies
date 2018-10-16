925c7fc2fa1e3e55b01b334a07d62e03b2f31349
Silvia Sanz <silviasanz3@yahoo.fr>
2018-10-16 20:31:00 +0200

    sauvegarde

---
+++ cart.php
@@ -0,0 +1,23 @@
+
+<?php require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+
+
+        <div class="col-4">
+            <div class="card" style="width:18rem">
+                <img class="card-img-top" src=" " alt="Card image cap" >
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text"></p>
+
+                </div>
+            </div>
+
+
+
+
+        TODO : Display shopping cart items from $_SESSION here.
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
